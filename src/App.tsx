879488c95bd8d82de1a3f26795d99c787e5f41ff
Moufaya <ayamoufidamessaoudi@gmail.com>
2025-09-16 12:55:22 +0000

    chore: init React (Vite) in Codespaces

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Bonjour 👋 Je lance React dans le navigateur !</h1>
+      <p>Aucune installation sur mon PC. Tout se passe dans Codespaces.</p>
+    </div>
+  );
+}
+
+export default App;
